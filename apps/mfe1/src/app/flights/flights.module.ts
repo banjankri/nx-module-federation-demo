3383ff0cbaba348eafd81d06416d0c2cd075deb3
Artur Banaś <banas.artur@gmail.com>
2023-03-01 17:24:23 +0100

    Update flights.module.ts

--- apps/mfe1/src/app/flights/flights.module.ts
+++ apps/mfe1/src/app/flights/flights.module.ts
@@ -3,12 +3,10 @@ import { CommonModule } from '@angular/common';
 import { FlightsSearchComponent } from './flights-search/flights-search.component';
 import { RouterModule } from '@angular/router';
 import { FLIGHTS_ROUTES } from './flights.routes';
-import { AuthLibModule } from '@demo/auth-lib';
 
 @NgModule({
   imports: [
     CommonModule,
-    AuthLibModule,
     RouterModule.forChild(FLIGHTS_ROUTES)
   ],
   declarations: [

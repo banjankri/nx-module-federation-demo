e9da5d7a7e8136ff03038e45bb440395f84ae57b
Artur Banaś <banas.artur@gmail.com>
2023-03-01 17:25:42 +0100

    Update app.routes.ts

--- apps/shell/src/app/app.routes.ts
+++ apps/shell/src/app/app.routes.ts
@@ -1,19 +1,22 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { AuthService } from '@demo/auth-lib';
 
 export const APP_ROUTES: Routes = [
     {
       path: '',
       component: HomeComponent,
-      pathMatch: 'full'
+      pathMatch: 'full',
+      providers: [ AuthService ]
     },
 
     // Your route here:
 
     {
       path: 'flights',
-      loadChildren: () => import('mfe1/Module').then(m => m.FlightsModule)
+      loadChildren: () => import('mfe1/Module').then(m => m.FlightsModule),
+      providers: [ AuthService ]
     },
 
     {
